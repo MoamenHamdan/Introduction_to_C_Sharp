6f2eaa0b0aebe51014816c40c741f4fa59a647cc
maomen hamdan <110568607+MoamenHamdan@users.noreply.github.com>
2025-01-23 22:35:44 +0200

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Class1
+    {
+ 
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your midterm grade :"); 
+            string input = Console.ReadLine();     
+            int midterm = int.Parse(input);         
+            Console.WriteLine("Enter your Final grade : ");
+            int final = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter your lab grade : ");
+            int lab = Convert.ToInt32(Console.ReadLine());  
+            int total = midterm + lab + final ;
+            Console.WriteLine("Your Course mark = " + total); 
+
+        }
+    }
+
+
+}
