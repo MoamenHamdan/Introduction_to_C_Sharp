86d26879b4cbc290d7daf076225c229d4f0310f1
maomen hamdan <110568607+MoamenHamdan@users.noreply.github.com>
2025-01-23 22:26:10 +0200

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Class2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose 1 or 2 : \n\t1.From C to F\n\t2.From F to C");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Temp : ");
+            int temp = Convert.ToInt32(Console.ReadLine());
+            switch (number) {
+                case 1:
+                    Console.WriteLine(temp + " Fahrenheit is " + (((temp - 32) * 5) / 9) + " Celsius ");
+                    break;
+                case 2:
+                    Console.WriteLine(temp + " Celsius is " + (((temp - 32) / 1.8)) + " Fahrenheit");
+                    break;
+
+            }
+        }
+    }
+}
---
+++ Class3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    internal class Class3
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Enter the number of courses you have : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            int sumOfCredits = 0;
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine("Enter the name of the course : ");
+                string name = Console.ReadLine();
+                Console.WriteLine("Enter the number of " + name );
+                int credits = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the Grade of " + name);
+                int grade = Convert.ToInt32(Console.ReadLine());
+                sum += credits * grade;
+                sumOfCredits += credits;
+            }
+            double gpa =(double) sum / (sumOfCredits * 25);
+            Console.WriteLine("Your total GPA is : " + gpa);
+
+        }
+    }
+}
+ 
